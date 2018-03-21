c0e6079f66b276db91d4959a75b3ff2b01236188
github username <github email address>
2018-03-21 11:31:29 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,22 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  function fetchData() {
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(json => console.log(json))
+    //.then( json => render(json['results'][0]))
+  }
+
+
+  //fetchData needs a function to render
+
+
+  let button = document.querySelector('.btn.btn-primary')
+  console.log(button)
+
+  button.addEventListener('click', function(){
+    fetchData()
+  })
 });
